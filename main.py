3c131fa8075eb5389278137b3ba1622780545096
zaber8787 <zaber8787@gmail.com>
2024-04-08 03:14:10 +0800

    music

--- main.py
+++ main.py
@@ -9,17 +9,49 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 # 遊戲初始化 & 創建視窗
 pygame.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("first game")
+
+
+# load image
 background_img = pygame.image.load(
     os.path.join('img', 'background.png')).convert()
 player_img = pygame.image.load(
     os.path.join('img', 'player.png')).convert()
-rock_img = pygame.image.load(
-    os.path.join('img', 'rock.png')).convert()
 bullet_img = pygame.image.load(
     os.path.join('img', 'bullet.png')).convert()
+rock_imgs = []
+for i in range(7):
+    rock_imgs.append(pygame.image.load(
+        os.path.join('img', f'rock{i}.png')).convert())
+
+# load music
+bullet_shoot = pygame.mixer.Sound(
+    os.path.join('sound', 'shoot.wav'))
+expl_sound = [
+    pygame.mixer.Sound(
+        os.path.join('sound', 'expl0.wav')),
+    pygame.mixer.Sound(
+        os.path.join('sound', 'expl1.wav'))
+]
+pygame.mixer.music.load(os.path.join('sound', 'background.ogg'))
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+# score board
+score = 0
+font_name = pygame.font.match_font('arial')
+
+
+def draw_score(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surf = font.render(text, True, WHITE)
+    text_rect = text_surf.get_rect()
+    text_rect.centerx = x
+    text_rect.top = y
+    surf.blit(text_surf, text_rect)
+
 # 物件Sprite
 
 
@@ -29,7 +61,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(player_img, (50, 38))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
+        self.rect.center = (WIDTH/2, HEIGHT*3/4)
         self.redius = 25
 
     def update(self):
@@ -55,21 +87,35 @@ class Player(pygame.sprite.Sprite):
         bullet = Bullet(self.rect.centerx, self.rect.top)
         all_sprite.add(bullet)
         bullets.add(bullet)
+        bullet_shoot.play()
 
 
 class Rock(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = rock_img
-        self.image.set_colorkey(BLACK)
+        self.image_ori = random.choice(rock_imgs)
+        self.image_ori.set_colorkey(BLACK)
+        self.image = self.image_ori.copy()
+
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
+        self.rect.y = random.randrange(-150, -00)
         self.speedy = random.randrange(2, 10)
         self.speedx = random.randrange(-3, 3)
-        self.radius = self.rect.width * 0.8
+        self.radius = self.rect.width * 0.8 / 2
+        self.degree = random.randrange(-3, 3)
+        self.sum = 0
+
+    def rotate(self):
+        self.sum += self.degree
+        self.sum %= 360
+        self.image = pygame.transform.rotate(self.image_ori, self.sum)
+        center = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = center
 
     def update(self):
+        self.rotate()
         self.rect.y += self.speedy
         self.rect.x += self.speedx
         if (self.rect.top >= HEIGHT or self.rect.left > WIDTH or self.rect.right < 0):
@@ -120,6 +166,8 @@ while running:
     all_sprite.update()
     hits = pygame.sprite.groupcollide(rocks, bullets, True, True)
     for hit in hits:
+        random.choice(expl_sound).play()
+        score += hit.radius
         rock = Rock()
         all_sprite.add(rock)
         rocks.add(rock)
@@ -131,5 +179,6 @@ while running:
     screen.fill(BLACK)
     screen.blit(background_img, (0, 0))
     all_sprite.draw(screen)
+    draw_score(screen, str(round(score)), 18, WIDTH * 1/2, 10)
     pygame.display.update()
 pygame.quit()
